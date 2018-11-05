c73d9eae3d6ffe0ef7c3ca6da895be003d51c291
Timothy <ygmpkk@gmail.com>
2018-11-05 13:56:39 +0800

    增加建议的LDAP MAIL方式登录

--- client.js
+++ client.js
@@ -1,14 +1,89 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
+import  { message, Modal, Icon, Input, Form, Button } from 'antd'
+import axios from 'axios'
+
+const FormItem = Form.Item
 
 module.exports = function (options) {
-  console.log(options)
+  this.bindHook('third_login', Form.create()(class LDAPComponent extends Component {
+    state = {
+      showLogin: false,
+    }
 
-  const handleLogin = () => {
-  }
+    handleClick = (e) => {
+      e.preventDefault()
 
-  const Component = () => (
-    <button onClick={handleLogin} className="btn-home btn-home-normal" >LDAP + Mail登录</button>
-  )
+      this.setState({
+        showLogin: true,
+      })
+    }
 
-  this.bindHook('third_login', Component);
+    doLogin = async (data) => {
+      const {headers} = await axios.post('/api/user/login_by_token', data)
+    }
+
+    handleLogin = (e) => {
+      e.preventDefault()
+
+      this.props.form.validateFields(async (err, values) => {
+        if (!err) {
+          const ret = await axios.post('/api/user/login_by_token', values)
+
+          // 没有更好的办法
+          if (/group/.test(ret.request.responseURL)) {
+            location.href = ret.request.responseURL
+            return
+          }
+
+          if (/html/.test(ret.headers['content-type'])) {
+            message.error('登录失败')
+            return
+          }
+        }
+      })
+    }
+
+    handleCancel = (e) => {
+      e.preventDefault()
+
+      this.setState({
+        showLogin: false,
+      })
+    }
+
+    render() {
+      const { getFieldDecorator } = this.props.form
+
+      return (
+        <Fragment>
+          <button onClick={this.handleClick} className="btn-home btn-home-normal" >LDAP + Mail登录</button>
+          <Modal
+            centered
+            maskClosable={false}
+            title='企业邮箱LDAP登录'
+            visible={this.state.showLogin}
+            onOk={this.handleLogin}
+            onCancel={this.handleCancel}
+          >
+            <Form>
+              <FormItem>
+                {getFieldDecorator('email', {
+                  rules: [{ required: true, message: '请输入电子邮箱' }],
+                })(
+                  <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Email" />
+                )}
+              </FormItem>
+              <FormItem>
+                {getFieldDecorator('password', {
+                  rules: [{ required: true, message: '请输入密码' }],
+                })(
+                  <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Password" />
+                )}
+              </FormItem>
+            </Form>
+          </Modal>
+        </Fragment>
+      )
+    }
+  }));
 };
--- server.js
+++ server.js
@@ -1,2 +1,47 @@
+const ldap = require('ldapjs')
+
 module.exports = function (options) {
+  const { server } = options
+  let msg = {}
+
+  this.bindHook('third_login', (ctx) => {
+    const { email, password } = ctx.request.body
+
+    return new Promise((resolve, reject) => {
+      const client = ldap.createClient({
+        url: server
+      })
+
+      client.once('error', err => {
+        if (err) {
+          msg = {
+            type: false,
+            message: `once: ${err}`
+          }
+
+          reject(msg)
+        }
+      })
+
+      client.bind(email, password, err => {
+        if (err) {
+          msg = {
+            type: false,
+            message: `登录失败`,
+          }
+          reject(msg)
+        } else {
+          msg = {
+            type: true,
+            message: `登录成功`,
+            email,
+            password,
+          }
+          resolve(msg)
+        }
+
+        client.unbind()
+      })
+    })
+  })
 }
