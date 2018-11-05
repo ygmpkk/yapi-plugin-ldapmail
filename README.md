ca5bc691db738050fc89e1c5fa73b11cd9529735
Timothy <ygmpkk@gmail.com>
2018-11-05 11:48:00 +0800

    add client demo

--- README.md
+++ README.md
@@ -1,31 +1,23 @@
-# yapi-plugin-qsso 
+# yapi-plugin-ldapmail
 
-第三方sso登录插件，配置方法如下：   
+企业邮箱LDAP登录插件，配置方法如下：   
 
 第一步： 在生成的配置文件config.json中加入如下配置：  
 
 ```
 "plugins": [
     {
-      "name": "qsso",
+      "name": "ldapmail",
       "options": {
-        "type": "sso",
-        "loginUrl": "http://sso.example.com/service/verifytoken.php?token=",
-        "emailPostfix": "@163.com",
-        "AUTH_SERVER" : "https://sso.example.com/login.php"
+        "server": "ldaps:/LDAP"
       }
     }
   ]
 ```   
-这里面的配置项含义如下：  
+这里面的配置项含义如下：
 
-- `type` 登陆类型，目前只支持sso登陆  
-- `loginUrl` 服务端在获取token之后，可以通过这个url来获取用户的详细信息
-- `emailPostfix` 登陆邮箱后缀
-- `AUTH_SERVER` 点击登陆按钮式需要跳转的url，用户通过该页面登录以后会向服务器发送一个token
+- `server` LDAP服务端
 
-
-第二步：在config.json 这层目录下运行 ```yapi plugin --name yapi-plugin-qsso```   重新下载插件  
+第二步：在config.json 这层目录下运行 ```yapi plugin --name yapi-plugin-ldapmail```   重新下载插件  
 
 第三步： 重启服务器
-
--- client.js
+++ client.js
@@ -1,36 +1,14 @@
 import React, { Component } from 'react'
 
-const qualifyURL = (url, encode) => {
-  url = url || '';
-  var ret = location.protocol + '//' + location.host + (url.substr(0, 1) === '/' ? '' : location.pathname.match(/.*\//)) + url;
-  if (encode) {
-    ret = encodeURIComponent(ret);
-  }
-  return ret;
-}
-
 module.exports = function (options) {
+  console.log(options)
+
   const handleLogin = () => {
-    const loginURI = '/api/user/login_by_token';
-    const { AUTH_SERVER } = options;
-    let ret = qualifyURL(loginURI, true);
-    let redirectURL = AUTH_SERVER  + '?ret=' + ret;
-    location.href = redirectURL;
   }
 
-  const QssoComponent = () => (
-    <button onClick={handleLogin} className="btn-home btn-home-normal" >QSSO 登录</button>
+  const Component = () => (
+    <button onClick={handleLogin} className="btn-home btn-home-normal" >LDAP + Mail登录</button>
   )
 
-  this.bindHook('third_login', QssoComponent);
+  this.bindHook('third_login', Component);
 };
-
-
-
-
-
-
-
-
-
-
